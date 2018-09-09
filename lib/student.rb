02816c423c022f7a5ee2d60b2dc201f29bbabc1f
Steven Inouye <steveninouye@msn.com>
2018-09-09 09:41:26 -1000

    added README.md

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
+# ## Student
+# * `Student#initialize` should take a first and last name.
+# * `Student#name` should return the concatenation of the student's
+#   first and last name.
+# * `Student#courses` should return a list of the `Course`s in which
+#   the student is enrolled.
+# * `Student#enroll` should take a `Course` object, add it to the
+#   student's list of courses, and update the `Course`'s list of
+#   enrolled students.
+#     * `enroll` should ignore attempts to re-enroll a student.
+# * `Student#course_load` should return a hash of departments to # of
+#   credits the student is taking in that department.
+
 class Student
 
 end
